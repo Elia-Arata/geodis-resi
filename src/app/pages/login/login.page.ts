5b983192c9df9c0711be36b1cea5ee4a7d792932
Elia Arata <elia93.arata@gmail.com>
2023-02-24 12:31:30 +0100

    loginPages basic structure

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth-service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  form = new FormGroup({
+    email: new FormControl('', { validators: [Validators.required] }),
+    password: new FormControl('', { validators: [Validators.required] }),
+  });
 
-  constructor() { }
+  constructor(private authService: AuthService) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+  login() {
+    if (this.form.invalid) return;
+
+    const { email, password } = this.form.getRawValue();
+    if (email && password) {
+      this.authService.login(email, password);
+    }
   }
-
 }
