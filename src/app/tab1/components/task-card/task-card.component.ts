d005fd6f62c95b0b55b2604c3d7f0db5cb8dff4c
Elia Arata <elia93.arata@gmail.com>
2023-02-24 14:46:59 +0100

    style

---
+++ src/app/tab1/components/task-card/task-card.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-task-card',
+  templateUrl: './task-card.component.html',
+  styleUrls: ['./task-card.component.scss'],
+})
+export class TaskCardComponent implements OnInit {
+@Input() nome?: string
+@Input() descrizione?: string
+@Input() stato?: boolean
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -6,7 +6,64 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+tasksList=[{
+  nome: 'task1',
+  descrizione: 'lorenipsum',
+  completed: true,
 
+},
+{
+  nome: 'task2',
+  descrizione: 'lorenipsum',
+  completed: false,
+
+},
+{
+  nome: 'task3',
+  descrizione: 'lorenipsum',
+  completed: true,
+
+},
+{
+  nome: 'task4',
+  descrizione: 'lorenipsum',
+  completed: false,
+
+},
+{
+  nome: 'task5',
+  descrizione: 'lorenipsum',
+  completed: true,
+
+},{
+  nome: 'task6',
+  descrizione: 'lorenipsum',
+  completed: false,
+
+},{
+  nome: 'task7',
+  descrizione: 'lorenipsum',
+  completed: true,
+
+},
+{
+  nome: 'task8',
+  descrizione: 'lorenipsum',
+  completed: false,
+
+},
+{
+  nome: 'task9',
+  descrizione: 'lorenipsum',
+  completed: true,
+
+},
+{
+  nome: 'task10',
+  descrizione: 'lorenipsum',
+  completed: false,
+
+}]
   constructor() {}
 
 }
